2f6cd87fb749c835762d1343e735f0ffeac62947
Tao <1987959014@qq.com>
2022-10-05 16:11:43 +0800

    增加了alarm的采集信息，修改图形界面。

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,23 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.os.Debug;
 
-public class MainActivity extends AppCompatActivity {
-    public static  final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static final String TAG = "Main";
+
+
+    // Collector
+    private static String mDir;
+    private static String output_filename;
+
+    Button alarm_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.alarm_button = findViewById(R.id.button2);
+        this.alarm_button.setOnClickListener(this);
     }
 
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.button2:
+                this.mDir = String.valueOf(this.getFilesDir());
+                this.output_filename = this.mDir+"/"+"test1.log";
+                Log.d(TAG, "onClick: ");
+                try {
+                    FileOutputStream out1 = new FileOutputStream(output_filename);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                FileDescriptor fd = new FileDescriptor();
+                android.os.Debug.dumpService("Alarm",fd,null);
+        }
     }
 }
