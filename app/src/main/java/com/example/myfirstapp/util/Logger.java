ffa3aea07ed089ca755cddd36e99dbbb975f0f70
Tao <1987959014@qq.com>
2022-10-10 17:59:37 +0800

    10.10日常更新

--- app/src/main/java/com/example/myfirstapp/util/Logger.java
+++ app/src/main/java/com/example/myfirstapp/util/Logger.java
@@ -1,11 +1,15 @@
 package com.example.myfirstapp.util;
 
 import android.util.Log;
+import android.util.Patterns;
 
+import java.io.BufferedReader;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Logger {
     //最终写入文件
@@ -14,8 +18,10 @@ public class Logger {
     //临时存放文件
     private String tmp_log_path ;
 
+    private String result_log_path;
+
     // 默认文件名
-    private String default_log_path = "result.log";
+    private String default_result_log_path = "result.log";
     private String default_tmp_log_path = "tmp.log";
     //当前路径
     private String mDir;
@@ -24,6 +30,8 @@ public class Logger {
 
     public Logger(String dir) {
         this.mDir = dir;
+        this.tmp_log_path = this.mDir+"/"+this.default_tmp_log_path;
+        this.result_log_path = this.mDir+ "/" + this.default_result_log_path;
     }
 
     // 导出log文件
@@ -35,11 +43,11 @@ public class Logger {
         args[0] = "--prot";
 
         // 导出临时log文件路径
-        tmp_log_path = this.mDir+"/"+this.default_tmp_log_path;
+
         FileOutputStream out1 = null;
 
         try {
-            out1 = new FileOutputStream(tmp_log_path);
+            out1 = new FileOutputStream(this.tmp_log_path);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -54,6 +62,34 @@ public class Logger {
 
     // 更新最终文件
     public void updateResult() throws IOException{
+        BufferedReader in = new BufferedReader(new FileReader(this.tmp_log_path));
+        String line;
+
+        // 匹配pending alarm的正则表达式
+        String pendingPattern = ".*pending alarms:.*";
+
+        // 匹配非pending alarm的正则表达式
+        String lazyPattern = ".*LazyAlarmStore stats:.*";
+
+        // 匹配alarm信息的正则表达式
+        String alarmPattern = ".*[(RTC_WAKEUP)(RTC)(ELAPSED_REALTIME_WAKEUP)(ELAPSED_REALTIME)].*";
+
+        boolean pendingMatch = false;
+
+        while((line = in.readLine())!= null){
+            if(pendingMatch){
+                if(line)
+
+                // 当匹配完pending alarm的信息后，重置
+                if(Pattern.matches(lazyPattern,line)){
+                    pendingMatch = false;
+                }
+            }
+            else{
+                pendingMatch = Pattern.matches(pendingPattern,line);
+            }
+
+        }
 
     }
 }
